1f20d5ca9e63a165f76c0e795891d9137b05f5b1
Cole <cole.r.glover@gmail.com>
2021-12-30 11:00:04 -0800

    work in progress

--- challenge-19.0.js
+++ challenge-19.0.js
@@ -12,8 +12,11 @@ const organizeData = (listOfReceivedData) => {
   const groupData = {};
 
   listOfReceivedData.forEach((i) => {
-    groupData[i.type] = groupData[i.type] || [];
+    groupData[i.type] = groupData[i.type] || []; 
     groupData[i.type].push(i.data);
+
+    // if the Object keys are not equal, create a new key-value array based on the new type
+    // add the corresponding value data to that array  
   });
 
   return groupData;
