0d02201774a8034c28eb52caca2858990c19c466
Cole <cole.r.glover@gmail.com>
2021-12-30 10:19:27 -0800

    completed challenge 19 and reformatted code

--- challenge-19.0.js
+++ challenge-19.0.js
@@ -1,22 +1,22 @@
 const listOfReceivedData = [
-  {type: "astro", data: "Saturn Data"},
-  {type: "bio", data: "Space Potatoes"},
-  {type: "physics", data: "Lagrange Points"},
-  {type: "bio", data: "OMG Tardigrades"},
-  {type: "physics", data: "Material reflectivity"},
-  {type: "astro", data: "Mercury is not the hottest"},
+  { type: "astro", data: "Saturn Data" },
+  { type: "bio", data: "Space Potatoes" },
+  { type: "physics", data: "Lagrange Points" },
+  { type: "bio", data: "OMG Tardigrades" },
+  { type: "physics", data: "Material reflectivity" },
+  { type: "astro", data: "Mercury is not the hottest" },
   //{type: "astro", data: "Mercury is not the hottest222"},
-]
+];
 
 const organizeData = (listOfReceivedData) => {
-
   const groupData = {};
 
-  listOfReceivedData.forEach(i => {    
-    groupData[i.type] = (groupData[i.type] || []);
-    groupData[i.type].push(i.data)
-  })
-  console.log(groupData)
-}
+  listOfReceivedData.forEach((i) => {
+    groupData[i.type] = groupData[i.type] || [];
+    groupData[i.type].push(i.data);
+  });
+
+  return groupData;
+};
 
 organizeData(listOfReceivedData);
