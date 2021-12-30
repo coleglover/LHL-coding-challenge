61792697d4d251901978a15dbc428706f464f63c
Cole <cole.r.glover@gmail.com>
2021-12-30 02:32:23 -0800

    Created a function that combines similar key-values

---
+++ challenge-19.0.js
@@ -0,0 +1,21 @@
+const listOfReceivedData = [
+  {type: "astro", data: "Saturn Data"},
+  {type: "bio", data: "Space Potatoes"},
+  {type: "physics", data: "Lagrange Points"},
+  {type: "bio", data: "OMG Tardigrades"},
+  {type: "physics", data: "Material reflectivity"},
+  {type: "astro", data: "Mercury is not the hottest"},
+  //{type: "astro", data: "Mercury is not the hottest222"},
+]
+
+const organizeData = (listOfReceivedData) => {
+
+  const groupData = {};
+
+  listOfReceivedData.forEach(i => {    
+    groupData[i.type] = (groupData[i.type] || [i.data])
+  })
+  console.log(groupData)
+}
+
+console.log(organizeData(listOfReceivedData));
